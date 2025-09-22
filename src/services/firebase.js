f09c342462acc35bafd403cfc3cc38fa4510dd54
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-09-22 12:57:08 -0500

    firebase service

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+// src/services/firebase.js
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export { signInWithPopup };
