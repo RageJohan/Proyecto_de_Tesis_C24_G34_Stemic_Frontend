b642609103f50fbb6f2c4f9eddd7aa5bcbe3416d
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-09-26 01:30:28 -0500

    App update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import AppRouter from './routes/AppRouter';
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
+import { LoaderProvider } from './context/LoaderContext';
+import Loader from './components/Loader';
+import React from 'react';
+import { useLoader } from './context/LoaderContext';
+
+function GlobalLoader() {
+  const { loading } = useLoader();
+  return loading ? <Loader /> : null;
+}
 
 function App() {
   return (
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <LoaderProvider>
+      <AuthProvider>
+        <GlobalLoader />
+        <AppRouter />
+      </AuthProvider>
+    </LoaderProvider>
   );
 }
 
